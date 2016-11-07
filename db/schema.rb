d648caaa8fcd07f379f4b4d166dc753f1a36c90d
root <root@c26a775c689b.(none)>
2016-11-07 15:39:02 +0000

    add update

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161107125520) do
+
+  create_table "hosts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "uuid"
+    t.string   "hostname"
+    t.string   "hostgroup"
+    t.string   "ipaddress"
+    t.string   "network"
+    t.string   "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
